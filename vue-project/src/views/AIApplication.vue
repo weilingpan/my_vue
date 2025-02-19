ca1d356c12161439bd488d25944cfee0fefd219f
WeiLing Pan <zz860530@gmail.com>
2025-02-20 01:15:05 +0800

    docs: update image

--- vue-project/src/views/AIApplication.vue
+++ vue-project/src/views/AIApplication.vue
@@ -5,7 +5,7 @@
   <!-- dify agent -->
   <iframe
     src="http://localhost/chatbot/jUAz8d4rh5hVAei4"
-    style="width: 100%; height: 100%; min-height: 700px"
+    style="width: 250%; height: 100%; min-height: 700px"
     frameborder="0"
     allow="microphone">
    </iframe>
